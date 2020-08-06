9792f3b642eeffa5216c1dbfee4496ed32cab40e
sanketshah19 <sanketshah17.ss@gmail.com>
2020-08-06 23:00:21 +0530

    Updates

---
+++ precache-manifest.06511c38ba6917e0f3dbeb8e783c4619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4c7cedd2fc48bf83560f42a23e357c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac9ac83375afb6350fda",
+    "url": "/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "939df089c7ca1eae1f36",
+    "url": "/static/css/main.3cce7f06.chunk.css"
+  },
+  {
+    "revision": "ac9ac83375afb6350fda",
+    "url": "/static/js/2.eec5f6b2.chunk.js"
+  },
+  {
+    "revision": "e62e25261b65f107a2413f59b77b2bd4",
+    "url": "/static/js/2.eec5f6b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "939df089c7ca1eae1f36",
+    "url": "/static/js/main.47873b52.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
